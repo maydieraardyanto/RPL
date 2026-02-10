75a8e95ec179e2586bb17f8cd139943d017136cf
Maydiera Ardyanto <maydieraevelyn@gmail.com>
2026-02-10 13:59:14 +0700

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -13,6 +13,10 @@ async function bootstrap() {
     }),
   );
   
-  await app.listen(process.env.PORT ?? 3000);
+  const PORT = process.env.PORT || 3000
+  app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`)
+})
+
 }
 bootstrap();
