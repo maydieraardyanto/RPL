17731a00e2f97dc65e7b6660873b549aa063e3dd
Maydiera Ardyanto <maydieraevelyn@gmail.com>
2026-02-10 13:34:08 +0700

    Update prisma service

--- src/prisma/prisma.service.ts
+++ src/prisma/prisma.service.ts
@@ -6,5 +6,6 @@ export class PrismaService extends PrismaClient
 implements OnModuleInit {
     async onModuleInit() {
         await this.$connect();
+        console.log('Connected to DB:', process.env.DATABASE_URL);
     }
 }
